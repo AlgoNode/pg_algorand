1eee39fbbc3d89ce029200b270bea34dd0a79a5c
Paweł Pierścionek <ppierscionek@gmail.com>
2022-02-16 00:21:02 +0100

    init

---
+++ address.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+
+	"github.com/algorand/go-algorand-sdk/types"
+	"gitlab.com/microo8/plgo"
+)
+
+//Meh prints out message to error elog
+func Meh() {
+
+	logger := plgo.NewErrorLogger("", log.Ltime|log.Lshortfile)
+	logger.Println("meh")
+}
+
+//AddressTxt2Bin converts account address in text form to binary form
+func AddressTxt2Bin(data string) []byte {
+	logger := plgo.NewErrorLogger("", log.Ltime|log.Lshortfile)
+	addr, err := types.DecodeAddress(data)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	return addr[:]
+}
+
+//AddressBin2Txt converts binary account address to text
+func AddressBin2Txt(data []byte) string {
+	logger := plgo.NewErrorLogger("", log.Ltime|log.Lshortfile)
+	addr, err := types.EncodeAddress(data)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	return addr
+}
