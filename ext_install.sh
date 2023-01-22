36f170aa0016fd7b6efe11269815064680aebf63
Paweł Pierścionek <ppierscionek@gmail.com>
2023-01-22 23:41:08 +0100

    nfd

--- ext_install.sh
+++ ext_install.sh
@@ -5,4 +5,5 @@ cd build
 /bin/mkdir -p '/usr/lib/postgresql/14/lib'
 /usr/bin/install -c -m 644 ./pg_algorand.control '/usr/share/postgresql/14/extension/'
 /usr/bin/install -c -m 644 ./pg_algorand--0.1.sql  '/usr/share/postgresql/14/extension/'
+/usr/bin/install -c -m 644 ./pg_algorand--0.1.sql  '/usr/share/postgresql/14/extension/pg_algorand--0.2.sql'
 /usr/bin/install -c -m 755  pg_algorand.so '/usr/lib/postgresql/14/lib/'
--- go.mod
+++ go.mod
@@ -4,10 +4,10 @@ go 1.18
 
 require (
 	github.com/algonode/plgo v1.2.4
-	github.com/algorand/go-algorand-sdk v1.20.0
+	github.com/algorand/go-algorand-sdk v1.24.0
 )
 
 require (
 	github.com/algorand/go-codec/codec v1.1.9 // indirect
-	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90 // indirect
+	golang.org/x/crypto v0.5.0 // indirect
 )
